cffc4b196a4e3be2e5b5fb6d8d4943342d26b9b7
Germán Carrillo <geotux_tuxman@linuxmail.org>
2016-08-02 21:04:08 -0500

    master prepared for new version 0.2.6.

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ def description():
   return "Automatic vector field updates when modifying or creating features"
 
 def version(): 
-  return "Version 0.2.5" 
+  return "Version 0.2.6" 
 
 def qgisMinimumVersion():
   return "2.0"
